0904132d091da1d33bd98bcaf4c931169edeb500
jjspscl <jjspscl@gmail.com>
2024-04-20 16:40:36 +0800

    1.4.0 | ALLOW PUT in CORS

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -16,7 +16,7 @@ const server = http.createServer(async (req, res) => {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT');
     res.setHeader('Access-Control-Allow-Headers', '*');
     if (req.method === 'OPTIONS') {
         res.writeHead(200);
