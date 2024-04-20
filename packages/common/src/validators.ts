76dcabd05ae8170e4ab9a42ca60022e114e463d8
jjspscl <jjspscl@gmail.com>
2024-04-20 19:06:20 +0800

    2.1.0 | MODIFY validator schema

--- packages/common/src/validators.ts
+++ packages/common/src/validators.ts
@@ -2,6 +2,6 @@ import z from 'zod';
 
 export const dutySchema = z.object({
     id: z.string().uuid().optional(),
-    name: z.string(),
+    name: z.string().min(3).max(32),
     completed: z.boolean().default(false),
 });
